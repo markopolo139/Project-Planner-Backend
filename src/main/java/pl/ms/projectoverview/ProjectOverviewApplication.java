0eab2b52cb255033c79c0ab1463ab5a7873174aa
markopolo139 <marekseget@onet.pl>
2023-09-14 22:41:49 +0200

    feat(utils): create function for retrieving current user

--- src/main/java/pl/ms/projectoverview/ProjectOverviewApplication.java
+++ src/main/java/pl/ms/projectoverview/ProjectOverviewApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //TODO:
 // scheduling to sent notification (use spring scheduling with function searching for projects with deadlines and notification enabled)
-// In services (for project (plan)) remember to set up user after conversion from app to persistence entity
 @SpringBootApplication
 public class ProjectOverviewApplication {
 
--- src/main/java/pl/ms/projectoverview/app/services/AppUtils.java
+++ src/main/java/pl/ms/projectoverview/app/services/AppUtils.java
@@ -2,9 +2,16 @@ package pl.ms.projectoverview.app.services;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import pl.ms.projectoverview.app.configuration.security.AppUserEntity;
+import pl.ms.projectoverview.app.exceptions.UserNotFoundException;
+import pl.ms.projectoverview.app.persistence.entities.UserEntity;
+import pl.ms.projectoverview.app.persistence.repositories.UserRepository;
 
 public class AppUtils {
     public static int getUserId() {
         return ((AppUserEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId();
+    }
+
+    public static UserEntity getCurrentUser(UserRepository userRepository) throws UserNotFoundException {
+        return userRepository.findById(getUserId()).orElseThrow(UserNotFoundException::new);
     }
 }
